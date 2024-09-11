a47d459ee31f789e2bdecab04ceee446710f9222
mrshane06 <niekerkkeegan@007gmail.com>
2024-09-11 14:29:00 +0200

    backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let port = process.env.PORT || 7070
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:8080/',
+    origin: 'http://localhost:8080',
     credentials : true
 }))
 app.use(express.json())
