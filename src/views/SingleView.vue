de9d603e30f593ac0abd46219db92cf230acd5c7
mrshane06 <niekerkkeegan@007gmail.com>
2024-09-12 14:57:47 +0200

    i hate my life

--- src/views/SingleView.vue
+++ src/views/SingleView.vue
@@ -1,47 +1,12 @@
 <template>
     <div v-if="book()">
         <div>
-            <h1>uy</h1>
+            <h1>i hate my life</h1>
             <a href="/products"></a><button>back</button>
         </div>
         <div v-if="book()">
             {{ $store.state.book.id}}
         </div>
-        <section class="section" id="product">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8">
-                    <div class="left-images">
-                        <img :src="book.bookURL" alt="">
-                    </div>
-                    </div>
-                    <div class="col-lg-4">
-                    <div class="right-content">
-                        <h4>{{ book.bookName }}</h4>
-                        <span class="price">${{ book.amount }}</span>
-                        <p>Quantity: {{ book.quantity }}</p>
-                        <p>Category: {{ book.category }}</p>
-                        <div class="quantity-content">
-                        <div class="left-content">
-                            <h6>No. of Orders</h6>
-                        </div>
-                        <div class="right-content">
-                            <div class="quantity buttons_added">
-                            <input type="button" value="-" class="minus" @click="decrementQuantity">
-                            <input type="number" step="1" min="1" max="" name="quantity" v-model="quantity" title="Qty" class="input-text qty text" size="4" pattern="" inputmode="">
-                            <input type="button" value="+" class="plus" @click="incrementQuantity">
-                            </div>
-                        </div>
-                        </div>
-                        <div class="total">
-                        <h4>Total: ${{ total }}</h4>
-                        <div class="main-border-button"><a href="#" @click="addToCart">Add To Cart</a></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </section>
     </div>
     <div v-else>
         <spinner-comp>
