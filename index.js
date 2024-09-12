361e24d64180f7dadd862dd6a2751fa267f24680
mrshane06 <niekerkkeegan@007gmail.com>
2024-09-12 14:46:23 +0200

    kjg

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let port = process.env.PORT || 7070
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://the-book-nook-72d3c.web.app',
     credentials : true
 }))
 app.use(express.json())
